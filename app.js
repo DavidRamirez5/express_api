95be351a6f09e7e4154fff9d0f2cb0c8ce234512
David Patiño <david.andrespr5@gmail.com>
2022-04-29 15:37:40 -0500

    4. Agregando un endpoint que regrese un explorar mediante un ID

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ app.get('/v1/explorers',(req,res)=>{
     const exploerers=[explorer1,explorer2,explorer3,explorer4]
     res.status(200).json(exploerers)
 })
-
+app.get('/v1/explorers/:id',(req,res)=>{
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id:1,name:'David'}
+    res.status(200).json(explorer)
+})
 
 // Con esto inicializamos esta app
 
